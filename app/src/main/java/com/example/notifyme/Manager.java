5366784c0ea54f3df71ff74067d232c155e8403e
phiphamdnvn <phiphamdnvn@gmail.com>
2021-06-23 16:47:00 +0700

    create a constructor for class Manager.java

--- app/src/main/java/com/example/notifyme/Manager.java
+++ app/src/main/java/com/example/notifyme/Manager.java
@@ -1,5 +1,13 @@
-public class Manager extends Employer{
-  private int salaryBase;
-  
-  
+package com.example.notifyme;
+
+import com.example.notifyme.Employer;
+
+public class Manager extends Employer {
+    private int salaryBase;
+
+
+    public Manager(int id, String name, double salary, int salaryBase) {
+        super(id, name, salary);
+        this.salaryBase = salaryBase;
+    }
 }
